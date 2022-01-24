f9b42ba16c4085bc5f8ab544b7711694c9737342
Abhinav <abhinav98jain@gmail.com>
2022-01-23 17:00:23 -0800

    Camera stuff

--- week3_cam.py
+++ week3_cam.py
@@ -9,19 +9,32 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import numpy as np
 from utils import run_command
+def region_of_interest(edges):
+    height, width = edges.shape
+    mask = np.zeros_like(edges)
+
+    # only focus bottom half of the screen
+    polygon = np.array([[
+        (0, height * 1 / 2),
+        (width, height * 1 / 2),
+        (width, height),
+        (0, height),
+    ]], np.int32)
+
+    cv2.fillPoly(mask, polygon, 255)
+    cropped_edges = cv2.bitwise_and(edges, mask)
+    return cropped_edges
 
 def detect_edges(frame):
     # filter for blue lane lines
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.imshow("hsv", hsv)
     lower_blue = np.array([90, 90, 50])
-    upper_blue = np.array([120, 180, 255])
+    upper_blue = np.array([120, 255, 255])
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    cv2.imshow("blue mask", mask)
-
+    
     # detect edges
     edges = cv2.Canny(mask, 200, 400)
-
+    edges = region_of_interest(edges)
     return edges
 
 if __name__ == "__main__":
