39b582c3c80fc94a5be97f70fbe105f03993a77a
Abhinav <abhinav98jain@gmail.com>
2022-02-05 17:16:59 -0800

    Week 5 - RossROS

---
+++ week5.py
@@ -0,0 +1,77 @@
+import sys
+sys.path.insert(0, "./lib/")
+# sys.path.insert(0, "./RossRos/")
+from lib.picarx_improved import *
+import time
+import concurrent.futures
+from readerwriterlock import rwlock
+from RossROS.rossros import *
+
+        
+class Sensor(object):
+    """Producer function for grayscale module"""
+    def __init__(self,  max = 1500, min = 750):
+        self.chn_0 = ADC("A0")
+        self.chn_1 = ADC("A1")
+        self.chn_2 = ADC("A2")
+        self.max =  max
+        self.min = min
+       
+    def get_grayscale_data(self):
+        adc_value_list = []
+        adc_value_list.append(self.chn_0.read())
+        adc_value_list.append(self.chn_1.read())
+        adc_value_list.append(self.chn_2.read())
+        for j,i in enumerate(adc_value_list):
+            if i > self.max:
+                adc_value_list[j]=self.max
+            elif i < self.min:
+                adc_value_list[j] = self.min
+    
+    def calibrate(self):
+        data = self.get_grayscale_data()
+        self.ref = sum(data)/len(data)
+        return self.ref
+
+class Interpretor():
+    """Consumer-Producer function to read sensor data and produce location wrt line"""
+    def __init__(self, sensitivity = 0.5, delay = 0.2, target = 'light'):
+        self.loc = 0
+        self.sensititvity = sensitivity
+        self.target = target
+        self.delay = delay
+    
+    def get_location(self, data):
+        #Uses the readings to calculate orientation of robot wrt line
+        loc = (0.6*(data[2] - data[0])/data[1] + 0.4*self.loc)*self.sensititvity
+        if self.target == 'dark':
+            loc = loc*-1
+        self.loc = loc
+    
+class Controller():
+    """Consumer function that reads location and controls the motors"""
+    def __init__(self, px_power):
+        self.px = Picarx()
+        self.px_power = px_power
+
+    def forward(self, data):
+        while True:
+            steering_angle = data*40
+            self.px.set_dir_servo_angle(steering_angle)
+            self.px.forward(self.px_power - abs(steering_angle)/4)
+
+if __name__=='__main__':
+  
+  try:
+    gm_sensor = Sensor()
+    interpret = Interpretor()
+    control = Controller(40)
+    gm_bus = Bus([0,0,0], name = "Input bus")
+    control_bus = Bus(0, name = "Output bus") 
+    gm_node = Producer(gm_sensor.get_grayscale_data, gm_bus, name = "Grayscale producer")
+    interpret_node = ConsumerProducer(interpret.get_location,gm_bus, control_bus, name = "Interpretor PC")
+    p_control_node = Consumer(control.forward, control_bus, name = "Control consumer")
+    node_list = [gm_node, interpret_node, p_control_node]
+    runConcurrently(node_list)
+  finally:
+    control.px.stop()
